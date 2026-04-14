326a3682c81f79e15a53e2d80336afd2983a8173
Assia <122679741+Assia123-oss@users.noreply.github.com>
2026-04-14 10:32:09 +0200

    Add check function to validate answer

---
+++ script.js
@@ -0,0 +1,7 @@
+function check(answer) {
+  if (answer === 4) {
+    document.getElementById("result").innerText = "Correct!";
+  } else {
+    document.getElementById("result").innerText = "Wrong!";
+  }
+}
